003c02647c9340a6c680524593d3ea30b6af7614
diaaanek <dkorongy@gmail.com>
2019-01-09 13:27:15 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,40 +17,33 @@ class ApplicationController < Sinatra::Base
     erb:new
   end
 
+
+get "/articles/new" do
+  @article = Article.new
+  erb:new
+end
 # new
 post '/articles' do
-  @articles = Article.create(params)
+  @article = Article.create(params)
   # (:title => params ['title'], :content => params ['content'])
   redirect "/article/#{@article.id}"
 end
 
-#show
 
 get 'articles/:id/edit' do
-  @article = Article.find_by_id(params['id'])
-  erb:show
+  @article = Article.find(params[:id])
+  erb:edit
 end
 
-# edit
+
 patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
   end
 
-  # show
-get "/articles/:id" do
-  @article = Article.find(params[:id])
-  erb :show
-end
 
-# edit
-get "/articles/:id/edit" do
-  @article = Article.find(params[:id])
-  erb :edit
-end
 
-  #destroy
   delete "/articles/:id" do
     Article.destroy(params[:id])
     redirect to "/articles"
